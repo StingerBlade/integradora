7537465f308c89d4227882273821a81f09ca240c
StingerBlade <arra2805200@gmail.com>
2025-03-13 19:39:53 -0600

    Remove confDB.py and apiDB.py from tracking

--- apiDB.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-uri = "mongodb+srv://valeria:valeria@integradora.rr5fz.mongodb.net/?retryWrites=true&w=majority&appName=Integradora"
-
-client = MongoClient(uri)
-db = client["valeria"]
-collection = db["usuarios"]
